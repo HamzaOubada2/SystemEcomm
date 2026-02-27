177009918105c18c8df974eda3e94919f4b36b1e
HamzaOubada2 <oubham220@gmail.com>
2026-02-27 15:06:20 +0000

    Refactor file upload handling and add workflow documentation

--- src/modules/carrier-reports/carrier-reports.controller.ts
+++ src/modules/carrier-reports/carrier-reports.controller.ts
@@ -5,10 +5,9 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { CarrierReportService } from './carrier-reports.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { UploadReportDto } from './dtos/upload-report.dto';
 
 @ApiTags('carrier-reports')
 @Controller('carrier-reports')
@@ -16,7 +15,9 @@ export class CarrierRaportsController {
   constructor(private readonly carrierReportsService: CarrierReportService) {}
 
   @Post('upload')
-  @ApiConsumes('multipart/form-data') //For know Swagger receive file upload type
+  @ApiConsumes('multipart/form-data') //For know Swagger Send file upload type
+
+  //You say to swagger i have field name file
   @ApiBody({
     schema: {
       type: 'object',
@@ -25,17 +26,16 @@ export class CarrierRaportsController {
       },
     },
   })
+
+  /* 
+    -> Receive file
+    -> Set in request.file
+    -> convert to Express.Multer.File
+  */
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File /* Receive file from request */,
+  ) {
     return this.carrierReportsService.processExcel(file);
   }
 }
-/*
-    Clinet -> Controller -> Service -> DB
-    - PORT /carrier-reports/upload
-        ->Interceptor: 
-            - Kychoof request
-            - if have name file
-            - save in memory
-            - set request.file
-*/
--- src/modules/carrier-reports/carrier-reports.service.ts
+++ src/modules/carrier-reports/carrier-reports.service.ts
@@ -11,56 +11,48 @@ export class CarrierReportService {
     private readonly reportRepository: Repository<CarrierReport>,
   ) {}
 
-  //Receive File From Multer
+  //Receive File from Controller
   async processExcel(file: Express.Multer.File) {
-    //If user not send file -> return 400
+    //If User Not send File -> return 400
     if (!file) {
       throw new BadRequestException('File is required');
     }
 
-    //Create Object in Memory present Excel File
+    // Create Null Object for Contains the Excel Content
     const workbook = new ExcelJS.Workbook();
 
-    //ExcelJs Read content of file file.buffer & convert Excel to Object Structure
-    //!Now Workbook have all sheets
+    //file comme from multer -> ExcelJs Read -> Conver tot structure have (sheets,rows,cells)
     await workbook.xlsx.load(file.buffer.buffer as ArrayBuffer);
 
-    //Get First Sheet
+    //Get First Sheet In Excel
     const worksheet = workbook.getWorksheet(1);
-    //If file have problem or no sheet return error
     if (!worksheet) {
       throw new BadRequestException('Worksheet not found');
     }
 
-    //Create Array  To assemble
-    //!we use partial because we dont enter all data of enity example we dont enter id createdAt ...
+    //We prepare an Array to stor rows in it before putting them in The database
     const reports: Partial<CarrierReport>[] = [];
 
-    //This Fct applies to evrty row in excel
+    //This function is passed on every line in Excel.
     worksheet.eachRow((row, rowNumber) => {
-      //Skipe first line because just have titles
+      //This condition for skip first line because have just Titles: (Tracking, Amount)
       if (rowNumber > 1) {
         reports.push({
           trackingNumber: row.getCell(1).value?.toString(),
           collectedAmount: Number(row.getCell(2).value),
           filename: file.originalname,
         });
+        /* 
+          tracking Number -> Column A
+          collectedAmount -> Column B
+          | A     | B   |
+          | ----- | --- |
+          | TRX01 | 500 |
+          | TRX02 | 800 |
+        */
       }
-      /* 
-        getCell(1) ==> column A(in excel)
-        getCell(2) ==> column B(in excel)
-
-        |TRX01|500|
-
-        {
-        trackingNumber: "TRX01",
-        collectedAmount: 500,
-        filename: "report.xlsx"
-        }
-      */
     });
-
-    //If Excel Null -> dont make insert
+    //check if Excel Empty
     if (reports.length === 0) {
       throw new BadRequestException('Excel Error');
     }
@@ -69,11 +61,11 @@ export class CarrierReportService {
   }
 }
 
-/* 
-    Role of this code:
-        -> Receives File Excel
-        -> Read
-        -> Extract Data
-        -> Convert data to Object
-        -> Save in database
+/*
+  MAIN IDEA OF THIS CODE:
+      -> Receive Excel File from user (using multer)
+      -> Read this File using ExcelJs(Librery)
+      -> Convert Rows to Object 
+      -> Save in database Using TypeORM
+      ! Excel -> JSON Object -> Database
 */
